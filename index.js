066d687f3997acda77d3a533c917dfcecbf8552c
Ben <ben@carbonorigins.com>
2022-05-17 12:30:03 +0100

    Added Agora cloud recording

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var http = require('http');
 var path = require('path');
 var puppeteer = require('puppeteer');
-var fetch = require('fetch');
+var request = require('request');
 var config = require('./config.json');
 
-var appId = '01c84bffc1d14fe3a6796d4e0726a4cb';
 var token = '';
 
-fetch.fetchUrl(`https://uldizaax95.execute-api.us-west-1.amazonaws.com/token?channel=${config.channel}`, function(err, meta, body){
-	console.log("token:", body.toString());
-	token = body.toString();
-
-	start();
-});
+if (config.appId == '') {
+	console.log('Config is missing app id');
+} else if (config.bucket.name == '') {
+	console.log('Config is missing bucket info');
+} else if (config.authKey == '') {
+	console.log('Config is missing auth key');
+} else {
+	request(`https://uldizaax95.execute-api.us-west-1.amazonaws.com/token?channel=${config.channel}`, function(error, response, body) {
+		console.log('Token: ' + body);
+		token = body;
+		start();
+	});
+}
 
 async function start() {
 	var browser = await puppeteer.launch({args: ['--no-sandbox', '--use-fake-ui-for-media-stream'], headless: true});
@@ -21,7 +26,7 @@ async function start() {
 		var page = await browser.newPage();
 		page.goto(`file://${path.dirname(require.main.filename)}/Agora-Web-Tutorial-1to1-Webpack/build/index.html`, {waitUntil: 'networkidle0'});
 		await page.waitForSelector('input#appID');
-		await page.type('input#appID', appId);
+		await page.type('input#appID', config.appId);
 		await page.type('input#channel', config.channel);
 		await page.type('input#token', token);
 
@@ -63,5 +68,136 @@ async function start() {
 
 		console.log(`Streaming camera ${i + 1}`);
 		// await page.screenshot({path: "screen.png"});
+	}
+	record();
+}
+
+function record() {
+	console.log('');
+	var resourceid;
+
+	var aquireBody = {
+		cname: config.channel,
+		uid: "527841",
+		clientRequest: {
+			resourceExpiredHour: 24,
+			scene: 0
+		}
+	}
+
+	console.log('Aquiring resource ID...');
+	request.post({
+		url: `https://api.agora.io/v1/apps/${config.appId}/cloud_recording/acquire`,
+		headers: {'content-type': 'application/json;charset=utf-8', 'Authorization': config.authKey},
+		body: JSON.stringify(aquireBody)
+	}, function(error, response, body) {
+		if (response.statusCode == 200) {
+			// console.log(JSON.parse(body));
+			resourceid = JSON.parse(body).resourceId;
+			console.log('Starting recording...');
+			startRecording();
+		} else {
+			console.log('Error!');
+			console.log(JSON.parse(body).message);
+		}
+	});
+
+	function startRecording() {
+		var recordBody = {
+			uid: "527841",
+			cname: config.channel,
+			clientRequest: {
+				token: token,
+				recordingConfig: {
+					maxIdleTime: 5,
+					streamTypes: 2,
+					audioProfile: 1,
+					channelType: 0,
+					videoStreamType: 0,
+					transcodingConfig: {
+						height: 1080,
+						width: 1920,
+						bitrate: 3150,
+						fps: 30,
+						mixedVideoLayout: 3,
+						layoutConfig: [
+							{
+								uid: "1",
+								x_axis: 0.0,
+								y_axis: 0.0,
+								width: 0.5,
+								height: 0.5
+							},
+							{
+								uid: "2",
+								x_axis: 0.5,
+								y_axis: 0.0,
+								width: 0.5,
+								height: 0.5
+							},
+							{
+								uid: "3",
+								x_axis: 0.0,
+								y_axis: 0.5,
+								width: 0.5,
+								height: 0.5
+							},
+							{
+								uid: "4",
+								x_axis: 0.5,
+								y_axis: 0.5,
+								width: 0.5,
+								height: 0.5
+							},
+						]
+					},
+					subscribeVideoUids: [
+						"1",
+						"2",
+						"3",
+						"4"
+					],
+					subscribeAudioUids: [
+						"1",
+						"2",
+						"3",
+						"4"
+					],
+					subscribeUidGroup: 0
+				},
+				recordingFileConfig: {
+					avFileType: ["hls", "mp4"] 
+				},
+				storageConfig: {
+					bucket: config.bucket.name,
+					region: config.bucket.region,
+					accessKey: config.bucket.accessKey,
+					secretKey: config.bucket.secretKey,
+					vendor: 1,
+					fileNamePrefix: [
+						config.channel.replace(/[^a-zA-Z0-9]/g, ""),
+						new Date().toISOString().substr(0,10).replace(/[^a-zA-Z0-9]/g, ""),
+						new Date().toISOString().substr(11,8).replace(/[^a-zA-Z0-9]/g, "")
+					]
+				}
+			}
+		}
+	
+		request.post({
+			url: `https://api.agora.io/v1/apps/01c84bffc1d14fe3a6796d4e0726a4cb/cloud_recording/resourceid/${resourceid}/mode/mix/start`,
+			headers: {'content-type': 'application/json;charset=utf-8', 'Authorization': config.authKey},
+			body: JSON.stringify(recordBody)
+		}, function(error, response, body) {
+			if (response.statusCode == 200) {
+			console.log('Recording started');
+				// console.log(response.statusCode);
+				// console.log(JSON.parse(body));
+				// console.log('resourceId: ' + JSON.parse(body).resourceId);
+				// console.log('sid: ' + JSON.parse(body).sid);
+			} else {
+				console.log('Error!');
+				console.log(JSON.parse(body).reason);
+			}
+		});
 	}
 }
